b798f543e5bdf18297d13483008c44a7457a5641
Bruno Nunes Cunha <brunonunes2505@gmail.com>
2024-07-15 10:11:29 -0300

    setimo

--- components/hero3.tsx
+++ components/hero3.tsx
@@ -1,13 +1,8 @@
-'use client'
 import { useEffect } from 'react';
 import gsap from 'gsap';
-import Image, { ImageProps } from 'next/image';
-import hero3 from '../public/hero3.svg';
-import hero4 from '../public/hero4.svg';
-import hero5 from '../public/hero5.svg';
 
 interface HeroImageProps {
-  src: ImageProps['src'];
+  src: string; // Alterei para 'string' para simplificar, você pode ajustar conforme necessário
   alt: string;
 }
 
@@ -15,11 +10,11 @@ const HeroImage: React.FC<HeroImageProps> = ({ src, alt }) => {
   useEffect(() => {
     const images = document.querySelectorAll('.hero-image');
 
-    const handleMouseEnter = (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    const handleMouseEnter = (event: MouseEvent) => {
       gsap.to(event.currentTarget, { scale: 1.1, duration: 0.3 });
     };
 
-    const handleMouseLeave = (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    const handleMouseLeave = (event: MouseEvent) => {
       gsap.to(event.currentTarget, { scale: 1, duration: 0.3 });
     };
 
@@ -39,30 +34,10 @@ const HeroImage: React.FC<HeroImageProps> = ({ src, alt }) => {
   return (
     <div className="relative w-full h-full mb-4">
       <div style={{ position: 'relative', width: '100%', height: '100%' }}>
-        <Image
-          src={src}
-          alt={alt}
-          layout="responsive"
-          objectFit="cover"
-          width={3000}
-          height={200}
-          className="hero-image"
-        />
+        <img src={src} alt={alt} className="hero-image" />
       </div>
     </div>
   );
 };
 
-export function Hero3(): JSX.Element {
-  return (
-    <div className="flex items-center justify-center min-h-screen">
-      <div className="flex flex-col items-center w-full max-w-screen-lg">
-        <div className="flex flex-col items-center">
-          <HeroImage src={hero3} alt="Hero 3" />
-          <HeroImage src={hero4} alt="Hero 4" />
-          <HeroImage src={hero5} alt="Hero 5" />
-        </div>
-      </div>
-    </div>
-  );
-}
+export default HeroImage;
