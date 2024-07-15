22df82a834f18710bfdaa3e2184895c522aa18a5
Bruno Nunes Cunha <brunonunes2505@gmail.com>
2024-07-15 10:12:53 -0300

    oitavo

--- components/hero3.tsx
+++ components/hero3.tsx
@@ -1,40 +1,28 @@
-import { useEffect } from 'react';
 import gsap from 'gsap';
 
 interface HeroImageProps {
-  src: string; // Alterei para 'string' para simplificar, você pode ajustar conforme necessário
+  src: string; // Ajuste conforme necessário
   alt: string;
 }
 
 const HeroImage: React.FC<HeroImageProps> = ({ src, alt }) => {
-  useEffect(() => {
-    const images = document.querySelectorAll('.hero-image');
+  const handleMouseEnter = (event: React.MouseEvent<HTMLDivElement>) => {
+    gsap.to(event.currentTarget, { scale: 1.1, duration: 0.3 });
+  };
 
-    const handleMouseEnter = (event: MouseEvent) => {
-      gsap.to(event.currentTarget, { scale: 1.1, duration: 0.3 });
-    };
-
-    const handleMouseLeave = (event: MouseEvent) => {
-      gsap.to(event.currentTarget, { scale: 1, duration: 0.3 });
-    };
-
-    images.forEach((image) => {
-      image.addEventListener('mouseenter', handleMouseEnter);
-      image.addEventListener('mouseleave', handleMouseLeave);
-    });
-
-    return () => {
-      images.forEach((image) => {
-        image.removeEventListener('mouseenter', handleMouseEnter);
-        image.removeEventListener('mouseleave', handleMouseLeave);
-      });
-    };
-  }, []);
+  const handleMouseLeave = (event: React.MouseEvent<HTMLDivElement>) => {
+    gsap.to(event.currentTarget, { scale: 1, duration: 0.3 });
+  };
 
   return (
     <div className="relative w-full h-full mb-4">
       <div style={{ position: 'relative', width: '100%', height: '100%' }}>
         <img src={src} alt={alt} className="hero-image" />
+        <div
+          className="absolute inset-0 cursor-pointer"
+          onMouseEnter={handleMouseEnter}
+          onMouseLeave={handleMouseLeave}
+        ></div>
       </div>
     </div>
   );
