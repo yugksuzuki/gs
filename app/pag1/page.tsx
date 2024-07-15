420a68abaa68d6addf1f3176862af856adee43bb
Bruno Nunes Cunha <brunonunes2505@gmail.com>
2024-07-15 12:01:32 -0300

    sec

--- app/pag1/page.tsx
+++ app/pag1/page.tsx
@@ -4,9 +4,6 @@ import { Footer } from "@/components/footer";
 export default function Home() {
   return (
     <>
-    <div>
-      teste
-    </div>
       <LogoIcon />
       <Footer />
     </>
--- components/logoIcon.tsx
+++ components/logoIcon.tsx
@@ -7,34 +7,6 @@ import logoIcon from "../public/logoIcon.svg";
 export function LogoIcon() {
   const containerRef = useRef<HTMLDivElement>(null);
 
-  useEffect(() => {
-    const containerElement = containerRef.current;
-
-    if (containerElement) {
-      const imageElement = containerElement.querySelector("img");
-
-      if (imageElement) {
-        const handleMouseEnter = () => {
-          gsap.to(imageElement, { scale: 1.1, duration: 0.3, ease: "power2.out" });
-          gsap.to(containerElement, { scale: 1.1, duration: 0.3, ease: "power2.out" });
-        };
-
-        const handleMouseLeave = () => {
-          gsap.to(imageElement, { scale: 1, duration: 0.3, ease: "power2.out" });
-          gsap.to(containerElement, { scale: 1, duration: 0.3, ease: "power2.out" });
-        };
-
-        containerElement.addEventListener("mouseenter", handleMouseEnter);
-        containerElement.addEventListener("mouseleave", handleMouseLeave);
-
-        return () => {
-          containerElement.removeEventListener("mouseenter", handleMouseEnter);
-          containerElement.removeEventListener("mouseleave", handleMouseLeave);
-        };
-      }
-    }
-  }, []);
-
   return (
     <section className="flex justify-center items-center h-screen">
       <div className="relative w-full max-w-[300px] md:max-w-[400px] lg:max-w-[500px]">
